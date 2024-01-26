087eed24f3f70d5dad2abe88cfebbf795c32fe1d
Juan <rojasjjrp272@gmail.com>
2024-01-26 13:08:22 -0500

    fibonnaci

--- Ejercicio1_RojasJuan.py
+++ Ejercicio1_RojasJuan.py
@@ -4,8 +4,22 @@ def fibonacci(n):
         sequence.append(sequence[-1] + sequence[-2])
     return sequence
 
+print("bienvenido usuario")
+print("ingresa un numero para crear la serie de fibonnaci")
+print("te explico, la serie fibonnaci es una secuencia de numeros")
+print("ejemplo: si colocas el numero 5, se van a sumar lo demas numeros que trae atras osea 0, 1, 1, 2, 3")
+ 
+
+
 n = int(input("ingresa el numero para la serie fibonnaci n: "))
 while n != 0:
     sequence = fibonacci(n)
     print(sequence)
-    n = int(input("ingresa el valor de n (0 to exit): "))
+    n = int(input("ingrese un nuevo número de datos a mostrar, o si quiere salir presione cero '0': "))
+
+
+
+
+
+
+
